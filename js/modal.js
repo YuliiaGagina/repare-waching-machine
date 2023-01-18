06188ceddd0136fb0834ee13f95d5310d8c1e36a
YuliiaGagina <ygaginazy@gmail.com>
2023-01-18 20:43:13 +0200

    finish with modal

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,15 @@
     openModalBtn: document.querySelectorAll("[data-modal-open]"  ),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    formEl: document.querySelector('.form'),
+    backdropEl: document.querySelector('.backdrop'),
   };
+
+  refs.backdropEl.addEventListener('click', (e)=>{
+    if(e.target === e.currentTarget){
+      refs.modal.classList.add("is-hidden");
+    }
+  })
 
   refs.openModalBtn.forEach(elem =>{
     elem.addEventListener("click", toggleModal)
